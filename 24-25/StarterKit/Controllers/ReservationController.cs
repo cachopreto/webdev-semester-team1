557b6e89807ddb338576ecc45706038fb2fb9f69
cachopreto <ghostpurp.0g@gmail.com>
2025-01-13 00:32:30 +0100

    man f this whole ish fr coi pndw ki unta sirbi pa sanka fr cnj sua

---
+++ 24-25/StarterKit/Controllers/ReservationController.cs
@@ -0,0 +1,11 @@
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using StarterKit.Services;
+
+namespace StarterKit.Controllers;
+
+[Route("api/v1/Reservation")]
+public class ReservationController : Controller {
+  
+  
+}
