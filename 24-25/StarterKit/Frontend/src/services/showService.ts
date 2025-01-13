d946ee7685fd19f8bdc90ee00ad799114ed23700
cachopreto <ghostpurp.0g@gmail.com>
2025-01-13 01:38:00 +0100

    init shii

---
+++ 24-25/StarterKit/Frontend/src/services/showService.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const fetchShows = async (filters: Record<string, string | undefined>) => {
+  try {
+    const response = await axios.get('/api/shows', {
+      params: { ...filters },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching shows:', error);
+    return [];
+  }
+};
+
+export const fetchVenues = async () => {
+  try {
+    const response = await axios.get('/api/venues');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching venues:', error);
+    return [];
+  }
+};
+
